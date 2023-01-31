861628aa8895f5441d555e44748ef277a5aeab9c
Roman Bielokon <belokon.roman@gmail.com>
2023-01-31 23:55:14 +0100

    implement AnimatedCounter

--- app/src/main/java/com/kvest/compose_sandbox/MainActivity.kt
+++ app/src/main/java/com/kvest/compose_sandbox/MainActivity.kt
@@ -3,13 +3,11 @@ package com.kvest.compose_sandbox
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import com.kvest.compose_sandbox.ui.animated_counter.AnimatedCounterPanel
 import com.kvest.compose_sandbox.ui.theme.ComposesandboxTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,26 +16,12 @@ class MainActivity : ComponentActivity() {
         setContent {
             ComposesandboxTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(
+                Box(
                     modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    AnimatedCounterPanel(modifier = Modifier.align(Alignment.Center))
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    ComposesandboxTheme {
-        Greeting("Android")
     }
 }
---
+++ app/src/main/java/com/kvest/compose_sandbox/ui/animated_counter/AnimatedCounter.kt
@@ -0,0 +1,84 @@
+package com.kvest.compose_sandbox.ui.animated_counter
+
+import androidx.compose.animation.*
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Button
+import androidx.compose.material.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun AnimatedCounterPanel(modifier: Modifier = Modifier) {
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = modifier
+    ) {
+        var count by remember { mutableStateOf(0) }
+
+        Button(onClick = { --count }) {
+            Text(text = "Dec")
+        }
+
+        Spacer(modifier = Modifier.width(24.dp))
+
+        AnimatedCounter(count = count)
+
+        Spacer(modifier = Modifier.width(24.dp))
+
+        Button(onClick = { ++count }) {
+            Text(text = "Inc")
+        }
+    }
+}
+
+private const val animationDurationMillis = 400
+
+@OptIn(ExperimentalAnimationApi::class)
+@Composable
+fun AnimatedCounter(
+    count: Int,
+    modifier: Modifier = Modifier,
+) {
+    //remember old value in order to calculate slide direction
+    var oldValue by remember { mutableStateOf(count) }
+
+    val slideDown = oldValue > count
+
+    LaunchedEffect(count) {
+        oldValue = count
+    }
+
+    val countStr = count.toString()
+
+    Row(modifier = modifier) {
+        for (i in -1  until countStr.length) {
+            key(countStr.length - i) {
+                AnimatedContent(
+                    targetState = countStr.getOrNull(i)?.toString() ?: "",
+                    transitionSpec =  {
+                        if (slideDown) {
+                            slideInVertically(animationSpec = tween(animationDurationMillis)) { hight -> hight } + fadeIn(animationSpec = tween(animationDurationMillis)) with slideOutVertically(animationSpec = tween(animationDurationMillis)) { hight -> -hight } + fadeOut(animationSpec = tween(animationDurationMillis))
+                        } else {
+                            slideInVertically(animationSpec = tween(animationDurationMillis)) { hight -> -hight } + fadeIn(animationSpec = tween(animationDurationMillis)) with slideOutVertically(animationSpec = tween(animationDurationMillis)) { hight -> hight } + fadeOut(animationSpec = tween(animationDurationMillis))
+                        }
+                        .using (
+                            SizeTransform(clip = false)
+                        )
+                    }
+                ) { value ->
+                    Text(
+                        text = value,
+                        fontSize = 32.sp,
+                        modifier = Modifier.defaultMinSize(
+                            minWidth = 16.dp
+                        )
+                    )
+                }
+           }
+        }
+    }
+}
