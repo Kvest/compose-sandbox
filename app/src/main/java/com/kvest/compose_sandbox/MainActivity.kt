2b4351377669be7fbb06457bef213ccac1b61970
Roman Bielokon <belokon.roman@gmail.com>
2023-03-15 21:37:10 +0100

    set Spline as main screen

--- app/src/main/java/com/kvest/compose_sandbox/MainActivity.kt
+++ app/src/main/java/com/kvest/compose_sandbox/MainActivity.kt
@@ -3,7 +3,9 @@ package com.kvest.compose_sandbox
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import com.kvest.compose_sandbox.ui.chess.ChessBoardDemo
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.ui.Modifier
+import com.kvest.compose_sandbox.ui.spline.Spline
 import com.kvest.compose_sandbox.ui.theme.ComposesandboxTheme
 
 class MainActivity : ComponentActivity() {
@@ -11,15 +13,9 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             ComposesandboxTheme {
-                ChessBoardDemo()
-
-                /*
-                Box(
-                    modifier = Modifier.fillMaxSize(),
-                ) {
-                    AnimatedCounterPanel(modifier = Modifier.align(Alignment.Center))
-                }
-                 */
+                Spline(
+                    modifier = Modifier.fillMaxSize()
+                )
             }
         }
     }
